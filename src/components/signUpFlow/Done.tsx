037ae35321d182cd22b27ffff90529b0327ea0b4
Ed Hughes <30260556+asparamancer@users.noreply.github.com>
2023-04-25 11:17:09 +0100

    adding more info to done page

--- src/components/signUpFlow/Done.tsx
+++ src/components/signUpFlow/Done.tsx
@@ -4,13 +4,19 @@ function Done({ email }) {
       <h1 class="text-4xl font-bold text-green-600 m-auto">Done!</h1>
       <p class="font-bold text-gray-700 m-auto mt-2">
         This was built by the Team at Visible. If you'd like to see more of what
-        we do, and get updates as we release new tools you can join the
+        we do you can click More Info, or if you'd like to get updates as we release new tools you can join the
         community below.
       </p>
-      <div class="bg-[#1bd4d4] my-4 max-w-fit justify-center flex rounded-md mx-auto">
+      <div class="my-4 max-w-fit justify-center flex rounded-md mx-auto gap-4">
+        <a
+          href={`https://www.visible.cx/join`}
+          class="fit-content rounded-md p-2 m-auto bg-[#1bd4d4]"
+        >
+          More Info
+        </a>
         <a
           href={`https://i.prefinery.com/projects/rjddzqda/users/instant?email=${email}&utm_source=removalTool&utm_campaign=removalTool`}
-          class="fit-content rounded-md p-2 m-auto"
+          class="fit-content rounded-md p-2 m-auto bg-[#1bd4d4]"
         >
           Join the community
         </a>
